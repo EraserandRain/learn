8ce63ed07284382e56be553591e5fcc1e17d24b9
EraserandRain <1349291258@qq.com>
2022-10-31 18:18:45 +0800

    test: Auto Deploy 20221031

--- auto_push.sh
+++ auto_push.sh
@@ -4,7 +4,7 @@ current_date=$(date -d '0 day' '+%Y%m%d')
 set -e
 git init
 git add -A
-git commit -m "feat: Auto Deploy ${current_date}"
+git commit -m "test: Auto Deploy ${current_date}"
 git push -f git@github.com:EraserandRain/code_learning.git main:main
 cd -
 echo 'deploy success!'
