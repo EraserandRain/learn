7c411c08254eb941002eba58322ce3d39fd98ea8
eraserandrain <1349291258@qq.com>
2023-07-06 17:19:07 +0800

    test: Auto Deploy bd3b308 at 20230706

--- perl/main.pl
+++ perl/main.pl
@@ -6,11 +6,20 @@ use Foo ':all';
 use File::Tee qw(tee);
 
 
-tee(STDOUT,'>','1.log');
-
-print("hello world\n");
-system('ls');
-
-
+my $build_server = `hostname`;
+$build_server =~ s/\n//g;
 # main
+# sub getMail(){
 
+# }
+print "================================================\n"
+  ."Start Build Step ... \n"
+  ."================================================\n";
+print "Build Server: ".$build_server."\n";
+
+sub getMail(){
+    print "haha\n";
+    print "Build Server: ".$build_server."\n";
+}
+
+getMail();
