f7f989795ace76f023a7a85f9e575d18484a9366
EraserandRain <1349291258@qq.com>
2023-09-22 14:57:12 +0800

    test: Auto Deploy 96fa4de at 20230922

--- perl/main.pl
+++ perl/main.pl
@@ -1,25 +1,15 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-BEGIN { unshift @INC,'module';};
+BEGIN { unshift @INC, 'module'; }
 use Foo ':all';
 use File::Tee qw(tee);
 
+my $version = "trunk";
+# my $version = "R7_1";
+# my $version = "R7_2";
+# my $version = "R6";
 
-my $build_server = `hostname`;
-$build_server =~ s/\n//g;
-# main
-# sub getMail(){
-
-# }
-print "================================================\n"
-  ."Start Build Step ... \n"
-  ."================================================\n";
-print "Build Server: ".$build_server."\n";
-
-sub getMail(){
-    print "haha\n";
-    print "Build Server: ".$build_server."\n";
+if ( $version =~ /^(R7_1|R7_2|trunk)$/ ) {
+    print("version: $version \n");
 }
-
-getMail();
