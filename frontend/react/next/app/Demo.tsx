82d741a6253431a46956b2831907ee3e4d4b6e2c
EraserandRain <1349291258@qq.com>
2023-11-07 18:08:20 +0800

    test: Auto Deploy 246b849 at 20231107

--- frontend/react/next/app/Demo.tsx
+++ frontend/react/next/app/Demo.tsx
@@ -1,7 +1,34 @@
-import { useState } from 'react';
+import React, { useReducer } from 'react';
+
+const countReducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return { count: state.count + 1 }
+        case 'dec':
+            return { count: state.count - 1 }
+        default:
+            return state
+    }
+}
 
 export default function Demo() {
+
+    const [state, dispatch] = useReducer(countReducer,{count: 0})
+
+    const inc = () => {
+        dispatch({ type: 'inc' })
+    }
+    const dec = () => {
+        dispatch({ type: 'dec' })
+    }
+
+
     return (
-        <p>Demo</p>
+        <>
+            <p>Count: {state.count}</p>
+            <button onClick={inc}>+</button>
+            <button onClick={dec}>-</button>
+        </>
     )
 }
+
