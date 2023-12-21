c67295ac1f4b8f6eac12daad8815e03a4f4beca3
EraserandRain <1349291258@qq.com>
2023-12-21 14:47:57 +0800

    test: Auto Deploy b6df59e at 20231221

--- frontend/react/next/next.config.js
+++ frontend/react/next/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const repo = 'learn'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: assetPrefix,
+    basePath: basePath,
+    output: 'export'
+}
 
 module.exports = nextConfig
