8e1dbb3a75c33165defc1c10e8770f923b25e9db
EraserandRain <1349291258@qq.com>
2024-03-08 15:45:03 +0800

    test: Auto Deploy baeb4dc at 20240308

--- perl/main.pl
+++ perl/main.pl
@@ -3,30 +3,40 @@ use strict;
 use warnings;
 use Time::HiRes qw(gettimeofday tv_interval);
 
-# Define a custom warning handler to handle warnings triggered by 'warn'.
-sub custom_warn_handler {
-    my ($warning) = @_;
-    $warning =~ s/at\s\S+ line \d+\.//;
-    print "[Warning]: $warning";
-}
-$SIG{__WARN__} = \&custom_warn_handler;
+# # Define a custom warning handler to handle warnings triggered by 'warn'.
+# sub custom_warn_handler {
+#     my ($warning) = @_;
+#     $warning =~ s/at\s\S+ line \d+\.//;
+#     print "[Warning]: $warning";
+# }
+# $SIG{__WARN__} = \&custom_warn_handler;
 
-warn("This is a custom warning.");
+# warn("This is a custom warning.");
 
+# #########################################################################
+# # Custom Info
+# # Define a custom subroutine to print informational messages.
+# #########################################################################
 
-#########################################################################
-# Custom Info
-# Define a custom subroutine to print informational messages.
-#########################################################################
+# sub info {
+#     my ($info_message) = @_;
+#     print "[Info]: $info_message\n";
+# }
 
-sub info {
-    my ($info_message) = @_;
-    print "[Info]: $info_message\n";
-}
+# # Example usage:
+# info("This is an info message.");
 
-# Example usage:
-info("This is an info message.");
+# print "start time: " . localtime(time) . "\n";
+# sleep(2);
+# print "end time: " . localtime(time) . "\n";
 
-print "start time: " . localtime(time) . "\n";
-sleep(5);
-print "end time: " . localtime(time) . "\n";
+my $isSuccess = 0;
+my $version = $isSuccess ? "R7_3":"trunk";
+# if ( $version =~ /^(R7_1|R7_2|trunk)$/ ) {
+#     my $build_server = `hostname`;
+#     $build_server =~ s/\n//g;
+#     # $build_server =~ s/[\n\s]+//g;
+#     print "Build Server: $build_server\n";
+# }
+print "version = $version \n";
+
