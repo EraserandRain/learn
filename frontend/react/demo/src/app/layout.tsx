5bf03b4d901db2329ea02bc621c518a6de98cec3
EraserandRain <1349291258@qq.com>
2024-03-28 16:04:21 +0800

    test: Auto Deploy 2c4cccc at 20240328

--- frontend/react/demo/src/app/layout.tsx
+++ frontend/react/demo/src/app/layout.tsx
@@ -16,7 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>
+          {children}
+        </main>
+        <footer>
+          <p>Copyright &copy; 2024</p>
+        </footer>
+      </body>
     </html>
   );
 }
